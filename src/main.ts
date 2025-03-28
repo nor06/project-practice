80f9d4b07f78ddf80f93504e46c7302d85a93fe8
nor06 <147531372+nor06@users.noreply.github.com>
2025-03-28 10:54:37 +0800

    Angular: Set Up Frontend and verify Connection to NestJs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+ 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+ 
+  app.enableCors({
+    origin: 'http://localhost:4200', // Allow Angular frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true, // Enable cookies if needed
+  });
+ 
+  await app.listen(3000);
 }
 bootstrap();
