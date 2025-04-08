d2791321cced6bb72e129a2b3e876ea1202eff8a
nor06 <147531372+nor06@users.noreply.github.com>
2025-04-08 12:11:41 +0800

    Angular Material DataTable & Server Side Pagination v2

--- frontend/src/app/services/authentication-service/authentication.service.ts
+++ frontend/src/app/services/authentication-service/authentication.service.ts
@@ -27,7 +27,7 @@ export class AuthenticationService {
 
   login(loginForm: LoginForm) {  
 
-    return this.http.post<any>('/api/users/login', {email: loginForm.email, password: loginForm.password}).pipe(
+    return this.http.post<any>('http://localhost:3000/users/login', {email: loginForm.email, password: loginForm.password}).pipe(
       map((token) => {
         console.log('token');
         localStorage.setItem('blog-token', token.access_token);
@@ -37,7 +37,7 @@ export class AuthenticationService {
   }
 
   register(user: User) {
-    return this.http.post<any>('/api/users', user).pipe(
+    return this.http.post<any>('http://localhost:3000/users', user).pipe(
       tap(user => console.log(user)),
       map(user => user)
     )
